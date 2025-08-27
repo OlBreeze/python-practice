e4aaa936d3745d367389d0c792701d896e366e06
Olga <fmwmf@ukr.net>
2025-08-27 19:25:06 +0300

    new row

--- calculate_circle_values.py
+++ calculate_circle_values.py
@@ -1,5 +1,6 @@
 import math
 
+
 def calculate_circle_area(radius:float)->float:
     """A function that: takes the radius of a circle, and returns the area of the circle."""
     return math.pi * radius ** 2
